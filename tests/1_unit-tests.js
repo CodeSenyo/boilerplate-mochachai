16cf81539b7e264db9ad39fbb81d1a0bccd1b2fc
CodeSenyo <richmondamanfusenyo60@gmail.com>
2024-09-03 12:52:05 +0000

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -9,3 +9,11 @@ suite('Unit Tests', function () {
       assert.isNotNull(1, '1 is not null');
     });
    
+/** 1 - Use assert.isNull() or assert.isNotNull() to make the tests pass. **/
+test('#isNull, #isNotNull', function() {
+  assert.isNull(
+    null,
+    'this is an optional error description - e.g. null is null'
+  );
+  assert.isNotNull(1, '1 is not null');
+});
